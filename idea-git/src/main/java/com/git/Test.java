9a141a82d48ef8d8ba4d34b5f540c425b5363da7
zhangyongchao <1534450577@qq.com>
2020-04-21 09:47:19 +0800

    添加git测试工程到git

---
+++ idea-git/src/main/java/com/git/Test.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @author zhangyongchao
+ * @date 2020/4/21 9:44
+ * @description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("git-main");
+    }
+
+}
