c2b481f99a581fc18d8be7b93305d41e9b9ac3a9
zhangyongchao <zhangyongchao@agree.com.cn>
2020-04-23 17:07:31 +0800

    修改main方法

--- idea-git/src/main/java/com/git/Test.java
+++ idea-git/src/main/java/com/git/Test.java
@@ -1,5 +1,8 @@
 package com.git;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zhangyongchao
  * @date 2020/4/21 9:44
@@ -9,6 +12,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("git-main");
+        Map<String, String> data = new HashMap<>();
+        data.put("test","testValue");
+        System.out.println(data);
     }
 
 }
